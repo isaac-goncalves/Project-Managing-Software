c03fe28fa6d67562ece026ac3a0a17a0d125d3d7
Caroline S. Amarante <caroline.amarante1998@gmail.com>
2023-06-09 18:10:57 -0300

    added db config gitignore

--- taskWise/backend/database/database.php
+++ taskWise/backend/database/database.php
@@ -6,7 +6,7 @@ function getConnection()
     $host = "localhost";
     $port = "5432";
     $database = "gerenciamento_de_atividades";
-    $user = "postgres";
+    $user = "carolineamarante";
     $password = "2406";
     $dsn = "pgsql:host=$host;port=$port;dbname=$database;user=$user;password=$password";
     $options = [
