d031e638b0790b4d1f2f72267d509cc166b4411c
Caroline S. Amarante <caroline.amarante1998@gmail.com>
2023-06-09 21:42:50 -0300

    Revert "added many stylesheets"
    This reverts commit 42edfb213707326705b343e09a4f1a10700a4d2d.

--- taskWise/creditos.php
+++ taskWise/creditos.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-  
+    <script type="text/javascript" src="./creditos.js"></script>
     <!-- <link rel="stylesheet" href="styles/tasks.css"> -->
     <title>Creditos</title>
     <style>
@@ -50,6 +50,7 @@
             <?php } ?>
         </div>
     </div>
+
 </body>
 
 </html>
